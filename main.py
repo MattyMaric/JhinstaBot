0ba01f23d102cdc475bcd7e4885866ba599c9034
MattyMaric <63099342+MattyMaric@users.noreply.github.com>
2020-09-24 14:53:12 +0200

    Update main.py
    Major changes to the code:
     -Shortened xpath paths so that it's easier to read
     -Changed find element line from xpath to name on username and password searches
     -Now asks for the username of the account you want to send messages to instead of the first person in your chat list
     -Added confirmation before starting the bot 
     -Some other small tweaks...

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
-from secrets import email, password
-from Quotes import JhinQuotes
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 from random import choice
+from secrets import email, password
+from Quotes import JhinQuotes
+
+YesAnswers = ['y', 'Y', 'YES','Yes', 'yes']
+NoAnswers = ['n', 'N', 'NO','No', 'no']
+
+
+
+print('Write the Instagram username of the person you want to send Jhin quotes to')
+recipient = input('Don\'t forget the dots(.) or underscores (_) in the username ')
+
+print('You are going to send Jhin quotes to ' + recipient)
+Confirmation = input('Are you sure? Y/N ')
+
+
+
 
 class JhinstaBot():
     def __init__(self, email, password):
@@ -18,32 +32,34 @@ class JhinstaBot():
     def SignIn(self):
         self.browser.get('https://www.instagram.com/accounts/login/')
         sleep(2)
-        emailInput = self.browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input') 
-        passwordInput = self.browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')
+        emailInput = self.browser.find_element_by_name('username') 
+        passwordInput = self.browser.find_element_by_name('password')
         emailInput.send_keys(self.email)
         
         passwordInput.send_keys(self.password)
         passwordInput.send_keys(Keys.ENTER)
         sleep(3)  
 
-    #Presses the "not now" buttons that appear after the login    
+    #Presses the "not now" buttons that appear for password saving and desktop notifications   
     def PopUp(self):
-        sleep(2)
-        NotNow = self.browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
-        NotNow.click() 
+        sleep(3)
+        NotNow = self.browser.find_element_by_xpath('//button[text()="Not Now"]')
+        NotNow.click()
     
     def PopUp2(self):
-        sleep(2)
-        NotNow2 = self.browser.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')
+        sleep(3)
+        NotNow2 = self.browser.find_element_by_xpath('//button[text()="Not Now"]')
         NotNow2.click()
 
-    # DM_Access Goes into the DM page on Instagram and selects the first person on the list (Recipient) and opens the chat history
+    #The bot takes the username given at the beginning and finds the profile by combining the username
+    # with instagrams link, after that it searches the page for the message button and clicks it
     def DM_Access(self):
-        sleep(2)
-        DirectMessage = self.browser.get('https://www.instagram.com/direct/inbox/')
-        sleep(2)
-        Recipient = self.browser.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[1]/a/div/div[2]/div[1]/div/div/div/div')
-        Recipient.click()
+        Rec_Insta_Page = 'https://www.instagram.com/' + recipient + '/'
+        self.browser.get(Rec_Insta_Page)
+        sleep(3)
+        MessageButton = self.browser.find_element_by_xpath('//button[text()="Message"]')
+        MessageButton.click()
+        
 
     #Main part of the code, selects the empty chat bar on the bottom, writes a random Jhin quote from the Quotes file and sends it
     # After that the bot waits 44 seconds (Because haha 4) before doing it all over again, and again, and again...
@@ -57,10 +73,21 @@ class JhinstaBot():
         
           
 
+def execution():
+    bot = JhinstaBot(email, password)
+    bot.SignIn()
+    bot.PopUp2()
+    bot.PopUp()
+    bot.DM_Access()    
+    bot.SendLoop()
 
-bot = JhinstaBot(email, password)
-bot.SignIn()
-bot.PopUp2()
-bot.PopUp()
-bot.DM_Access()    
-bot.SendLoop()
+
+#If the user says yes to the promp at the beginning of the code, the code runs, otherwise it shuts down
+if Confirmation in YesAnswers:
+    execution()
+elif Confirmation in NoAnswers:
+    print('Okay, come back when you change your mind')
+    exit()
+else:
+    print('Invalid response given, shutting down...')
+    exit()
